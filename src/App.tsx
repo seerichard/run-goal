6b81e0fd8a6d8839e27ffc457ffb8ae141469252
Richard See <richard.see@argos.co.uk>
2021-01-11 18:19:19 +0000

    Get a basic form working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import { FC } from 'react';
-import './App.css';
+import { useForm } from 'react-hook-form';
 
-const App: FC = () => (
-  <div className="App">
-    <button>Hey click me</button>
-  </div>
-);
+type FormValues = {
+  clientId: string;
+  clientSecret: string;
+};
+
+const App: FC = () => {
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  const { register, errors, handleSubmit } = useForm<FormValues>();
+  const onSubmit = (data: FormValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Client Id</label>
+      <input name="clientId" ref={register({ required: true })} />
+      {errors.clientId && 'Please enter a Client Id'}
+
+      <label>Client Secret</label>
+      <input name="clientSecret" ref={register({ required: true })} />
+      {errors.clientSecret && 'Please enter a Client Secret'}
+
+      <input type="submit" />
+    </form>
+  );
+};
 
 export default App;
