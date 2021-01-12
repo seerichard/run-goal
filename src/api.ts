722e3c61f3f520b701c58efa6fd89e25915c7e1a
Richard See <richard.see@argos.co.uk>
2021-01-12 23:12:10 +0000

    Add getActivities API

--- src/api.ts
+++ src/api.ts
@@ -14,14 +14,14 @@ export const getTokens: Token = async (code: string) => {
   const clientId = localStorage.getItem(CLIENT_ID);
   const clientSecret = localStorage.getItem(CLIENT_SECRET);
 
-  const fetchResponse = await fetch(
+  const response = await fetch(
     `https://www.strava.com/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&code=${code}&grant_type=authorization_code`,
     {
       method: 'POST',
     },
   );
 
-  const data = await fetchResponse.json();
+  const data = await response.json();
   const {
     access_token,
     refresh_token,
@@ -36,4 +36,38 @@ export const getTokens: Token = async (code: string) => {
   const url = window.location.href;
 
   window.location.href = url.split('?')[0];
+};
+
+type VoidReturn = () => Promise<void>;
+
+export const refreshToken: VoidReturn = async () => {
+  const clientId = localStorage.getItem(CLIENT_ID);
+  const clientSecret = localStorage.getItem(CLIENT_SECRET);
+  const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+
+  const response = await fetch(
+    `https://www.strava.com/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`,
+    {
+      method: 'POST',
+    },
+  );
+
+  const data = await response.json();
+  const { access_token } = data;
+
+  localStorage.setItem(ACCESS_TOKEN, access_token);
+};
+
+export const getActivities: VoidReturn = async () => {
+  const accessToken = localStorage.getItem(ACCESS_TOKEN);
+  const RUN = 'Run';
+
+  const response = await fetch(
+    `https://www.strava.com/api/v3/athlete/activities?access_token=${accessToken}&per_page=200`,
+  );
+
+  const data = await response.json();
+  const runData = data.filter(({ type }: { type: string }) => type === RUN);
+
+  return runData;
 };
