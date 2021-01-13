82bb012880d26e812043f0fee5631623b5808d8d
Richard See <richard.see@argos.co.uk>
2021-01-13 00:44:19 +0000

    Total distance displayed (very messy)

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,17 +1,34 @@
-import { FC, useEffect } from 'react';
+import { FC, useState, useEffect } from 'react';
 import { refreshToken, getRuns } from '../api';
 
 const MainScreen: FC = () => {
+  const [runData, setRunData] = useState<any[]>([]);
+
   useEffect(() => {
     refreshToken().catch((error) =>
       console.log('Something happened refreshing the token :(', error),
     );
-    getRuns().catch((error) =>
-      console.log('Something happened getting activities :(', error),
-    );
+
+    getRuns()
+      .then((res) => setRunData(res))
+      .catch((error) =>
+        console.log('Something happened getting activities :(', error),
+      );
   }, []);
 
-  return <div>Display!</div>;
+  console.log('runData:', runData);
+
+  const totalDistance: number =
+    runData.reduce(
+      // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+      (acc, curr) => acc + curr.distance,
+      0,
+    ) / 10;
+
+  const totalDistanceKm =
+    Math.round(totalDistance + Number.EPSILON * 100) / 100;
+
+  return <div>Total Distance Run This Year: {totalDistanceKm}km</div>;
 };
 
 export default MainScreen;
