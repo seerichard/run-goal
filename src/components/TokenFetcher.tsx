015b9112287d569ce4e359c9587507501a4cbd01
Richard See <richard.see@argos.co.uk>
2021-01-17 16:41:46 +0000

    Styled TokenFetcher

--- src/components/TokenFetcher.tsx
+++ src/components/TokenFetcher.tsx
@@ -1,5 +1,40 @@
 import { FC, useEffect } from 'react';
+import styled from 'styled-components';
 import { getTokens } from '../api';
+import { ReactComponent as Puff } from '../images/puff.svg';
+import { media } from '../styles/breakpoints';
+import { white } from '../styles/colors';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Puffer = styled(Puff)`
+  width: 150px;
+  height: 150px;
+  margin-top: 100px;
+
+  @media ${media.cinema} {
+    width: 9.375vw;
+    height: 9.375vw;
+    margin-top: 6.25vw;
+  }
+`;
+
+const Text = styled.span`
+  color: ${white};
+  font-size: 28px;
+  margin: 0;
+  margin-top: 50px;
+  text-align: center;
+
+  @media ${media.cinema} {
+    font-size: 1.75vw;
+    margin-top: 3.125vw;
+  }
+`;
 
 type Props = {
   code: string;
@@ -12,7 +47,12 @@ const TokenFetcher: FC<Props> = ({ code }) => {
     );
   }, []);
 
-  return <div>Token Fetcher Component</div>;
+  return (
+    <Wrapper>
+      <Puffer />
+      <Text>Linking account...</Text>
+    </Wrapper>
+  );
 };
 
 export default TokenFetcher;
