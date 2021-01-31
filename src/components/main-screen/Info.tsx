27e927e37c9877f20cf64330baa9371c83e49476
Richard See <richard.see@argos.co.uk>
2021-01-31 19:10:27 +0000

    Pass through Year To Date info

--- src/components/main-screen/Info.tsx
+++ src/components/main-screen/Info.tsx
@@ -1,11 +1,40 @@
 import type { FC } from 'react';
 import type { Activity } from '../../types/activity';
 import Goal from './Goal';
+import Card from './Card';
 
 type InfoProps = {
   runData: Activity[];
 };
 
-const Info: FC<InfoProps> = ({ runData }) => <Goal runData={runData} />;
+const Info: FC<InfoProps> = ({ runData }) => {
+  const runs = runData.length;
+  const movingTime = runData.reduce(
+    (acc, { moving_time }) => acc + moving_time,
+    0,
+  );
+
+  const h = Math.floor(movingTime / 3600);
+  const m = Math.floor((movingTime % 3600) / 60);
+
+  const elevationGain = runData.reduce(
+    (acc, { total_elevation_gain }) => acc + total_elevation_gain,
+    0,
+  );
+
+  return (
+    <>
+      <Goal runData={runData} />
+      <Card
+        title="Year To Date"
+        data={{
+          'Runs': runs,
+          'Time': `${h}h ${m}m`,
+          'Elevation Gain': `${elevationGain}m`,
+        }}
+      />
+    </>
+  );
+};
 
 export default Info;
