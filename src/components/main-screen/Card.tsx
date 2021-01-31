e95fdda64c385cbb251ebccdea93b08ce8fafbbf
Richard See <richard.see@argos.co.uk>
2021-01-31 17:29:53 +0000

    Format card better

--- src/components/main-screen/Card.tsx
+++ src/components/main-screen/Card.tsx
@@ -1,4 +1,4 @@
-import type { FC, ReactNode } from 'react';
+import type { FC, ReactText } from 'react';
 import styled from 'styled-components';
 import { media } from '../../styles/breakpoints';
 import { grey1 } from '../../styles/colors';
@@ -13,7 +13,10 @@ const CardWrapper = styled.div`
   border-radius: 3px;
 
   @media ${media.cinema} {
+    width: 18.75vw;
     margin-top: 3.75vw;
+    border: 0.0625vw solid ${grey1};
+    border-radius: 0.1875vw;
   }
 `;
 
@@ -23,23 +26,60 @@ const Title = styled.span`
   padding: 10px;
   font-size: 16px;
   font-weight: bold;
+
+  @media ${media.cinema} {
+    border-bottom: 0.0625vw solid ${grey1};
+    padding: 0.625vw;
+    font-size: 1vw;
+  }
 `;
 
 const Content = styled.div`
   color: ${grey1};
   padding: 10px;
   font-size: 14px;
+
+  @media ${media.cinema} {
+    padding: 0.625vw;
+    font-size: 1vw;
+  }
 `;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 3px;
+
+  @media ${media.cinema} {
+    margin-bottom: 0.1875vw;
+  }
+`;
+
+interface Data {
+  [key: string]: ReactText;
+}
+
+const formatData = (data: Data) =>
+  Object.entries(data).map((dataPoint) => {
+    const [name, value] = dataPoint;
+
+    return (
+      <InfoWrapper key={name}>
+        <span>{name}</span>
+        <span>{value}</span>
+      </InfoWrapper>
+    );
+  });
 
 type CardProps = {
   title: string;
-  children: ReactNode;
+  data: Data;
 };
 
-const Card: FC<CardProps> = ({ title, children }) => (
+const Card: FC<CardProps> = ({ title, data }) => (
   <CardWrapper>
     <Title>{title}</Title>
-    <Content>{children}</Content>
+    <Content>{formatData(data)}</Content>
   </CardWrapper>
 );
 
